63de51441accf0df4660ffc02544f0d339cba0f9
pigmore <pigmore@gmail.com>
2024-06-03 15:15:10 +0800

    no message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRef, useState, useEffect,createRef} from 'react';
+import axios from 'axios';
+
 
 function App() {
+  useEffect(() => {
+    async function init() {
+      if (typeof window !== "undefined" || window.initReady !== true) {
+        window.initReady = true
+        axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`)
+        .then(res => {
+          const listTemp = res.data.results;
+          console.log(listTemp)
+        })
+      }
+    }
+    init()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
