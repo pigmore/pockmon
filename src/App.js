b9ff8075b81b4a3e17692be20790a34d8136fcd5
pigmore <pigmore@gmail.com>
2024-06-03 15:25:03 +0800

    attach the base_state data

--- src/App.js
+++ src/App.js
@@ -8,40 +8,75 @@ function App() {
   const [page, setPage] = useState(1)
   const [visible, setVisible] = useState(0)
   const [totalCard, setTotalCard] = useState(0)
-
+  const loadBasicStates = async (index)=>{
+    axios.get(index)
+    .then(res => {
+      return res.data.stats;
+    })
+  }
   useEffect(() => {
     async function init() {
       if (typeof window !== "undefined" || window.initReady !== true) {
-
-        axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`)
-        .then(res => {
-          const listTemp = res.data.results;
-
-          setTotalCard(res.data.count)
-          setPokemonList(listTemp)
-          setVisible(listTemp.length)
-          window.initReady = true
-        })
+        loadMoreCards()
       }
     }
     init()
   }, [])
 
-  const loadMoreCards = ()=>{
-    axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`)
-    .then(res => {
-      const listTemp = res.data.results;
 
-      setPokemonList((prev)=>[...prev,...listTemp])
-      setVisible((prev)=>prev +listTemp.length)
-    })
+  const loadMoreCards = ()=>{
+
+    if (!window.isloading || window.initReady) {
+      window.isloading = true
+      axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${visible}&limit=20`)
+      .then(res => {
+        let listTemp = res.data.results;
+        // listTemp.map(async (item,index)=>{
+        //   item.state = await loadBasicStates(item.url)
+        // })
+        let promiseArray = []
+        listTemp.forEach((item, i) => {
+          promiseArray.push(axios.get(item.url))
+        });
+
+        Promise.all(promiseArray).then((res0) => {
+          window.isloading = false
+          listTemp.map((item,index)=>{
+            item.state = res0[index].data.stats
+          })
+          if (window.initReady) {
+            setPokemonList((prev)=>[...prev,...listTemp])
+            setVisible((prev)=>prev +listTemp.length)
+          }else{
+            setTotalCard(res.data.count)
+            setPokemonList(listTemp)
+            setVisible(listTemp.length)
+            window.initReady = true
+          }
+
+        });
+
+
+      })
+    }
   }
 
+  const statItems = (array) => {
+    let stateTemp = ''
+    array.forEach((_item, i) => {
+      stateTemp += `${_item.stat.name}:${_item.base_stat}`
+      stateTemp += '\n'
+    });
+    return stateTemp
+  }
   const listItems = pokemonList.map((item,index) =>
     <li key={index} className='pokemonCard'>
-      <img className='App-logo' src={logo} alt=""/>
+      <img className='App-logo' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${index+1}.png`} alt=""/>
       <br/>
-    {item.name}</li>
+      {item.name}
+      <br/>
+      {item.state && item.state.length > 0 ? statItems(item.state) : null}
+    </li>
   );
 
   const handleOnScroll = () => {
@@ -54,7 +89,7 @@ function App() {
     }
   };
   useEffect(() => {
-    loadMoreCards();
+    if (window.initReady) loadMoreCards();
   }, [page]);
 
   useEffect(() => {
@@ -69,7 +104,6 @@ function App() {
 
   return (
     <div className="App"
-      onClick={loadMoreCards}
     >
       {listItems}
     </div>
