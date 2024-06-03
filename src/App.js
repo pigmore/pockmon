8d84cfacd29212051628c5ffbfb593f780d073b8
pigmore <pigmore@gmail.com>
2024-06-03 15:26:51 +0800

    implement the search bar

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import axios from 'axios';
 
 function App() {
   const [pokemonList, setPokemonList] = useState([])
+  const [pokemonSearch, setPokemonSearch] = useState(null)
   const [page, setPage] = useState(1)
+  const [search, setSearch] = useState('')
   const [visible, setVisible] = useState(0)
   const [totalCard, setTotalCard] = useState(0)
-  const loadBasicStates = async (index)=>{
+  const loadBasicstats = async (index)=>{
     axios.get(index)
     .then(res => {
       return res.data.stats;
@@ -16,6 +18,7 @@ function App() {
   }
   useEffect(() => {
     async function init() {
+      window.timeout = null;
       if (typeof window !== "undefined" || window.initReady !== true) {
         loadMoreCards()
       }
@@ -24,6 +27,25 @@ function App() {
   }, [])
 
 
+  const searchCards = ()=>{
+    if (search.length < 1) return
+
+    if(window.timeout !== null)  clearTimeout(window.timeout);
+    setPokemonSearch(null)
+    window.timeout = setTimeout(
+      ()=>{
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${search}`)
+        .then(res => {
+          console.log(res.data)
+          setPokemonSearch(res.data)
+        })
+        .catch(function (error) {
+          console.log('Error', error.message);
+        })
+      }
+    , 1000);
+
+  }
   const loadMoreCards = ()=>{
 
     if (!window.isloading || window.initReady) {
@@ -32,7 +54,7 @@ function App() {
       .then(res => {
         let listTemp = res.data.results;
         // listTemp.map(async (item,index)=>{
-        //   item.state = await loadBasicStates(item.url)
+        //   item.stats = await loadBasicstats(item.url)
         // })
         let promiseArray = []
         listTemp.forEach((item, i) => {
@@ -42,7 +64,7 @@ function App() {
         Promise.all(promiseArray).then((res0) => {
           window.isloading = false
           listTemp.map((item,index)=>{
-            item.state = res0[index].data.stats
+            item.stats = res0[index].data.stats
           })
           if (window.initReady) {
             setPokemonList((prev)=>[...prev,...listTemp])
@@ -54,9 +76,14 @@ function App() {
             window.initReady = true
           }
 
-        });
+        })
+        .catch(function (error) {
+          console.log('Error', error.message);
+        })
 
-
+      })
+      .catch(function (error) {
+        console.log('Error', error.message);
       })
     }
   }
@@ -66,16 +93,62 @@ function App() {
     array.forEach((_item, i) => {
       stateTemp += `${_item.stat.name}:${_item.base_stat}`
       stateTemp += '\n'
+
     });
     return stateTemp
   }
+  const resultItem = ()=>{
+    return(
+      <li className='pokemonCard'>
+        <div>
+          <img className='App-logo' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${pokemonSearch.id}.png`} alt=""/>
+          {pokemonSearch.name}
+        </div>
+        <div>
+
+          {pokemonSearch.stats && pokemonSearch.stats.length > 0 ?
+            <div>
+              {pokemonSearch.stats.map((_item,index) => (
+                <tr key= {index} >
+                  <td>{
+                      _item.stat.name == 'special-attack' ? 'sp.atk':
+                      _item.stat.name == 'special-defense' ? 'sp.def':
+                      _item.stat.name
+                  }&nbsp;</td>
+                  <td>{_item.base_stat}</td>
+                </tr>
+              ))}
+            </div>
+          : null}
+        </div>
+
+      </li>
+    )
+  }
   const listItems = pokemonList.map((item,index) =>
     <li key={index} className='pokemonCard'>
-      <img className='App-logo' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${index+1}.png`} alt=""/>
-      <br/>
-      {item.name}
-      <br/>
-      {item.state && item.state.length > 0 ? statItems(item.state) : null}
+      <div>
+        <img className='App-logo' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${index+1}.png`} alt=""/>
+        {item.name}
+      </div>
+      <div>
+
+        {item.stats && item.stats.length > 0 ?
+          <tbody>
+            {item.stats.map(_item => (
+              <tr>
+                <td>{
+                    _item.stat.name == 'special-attack' ? 'sp.atk':
+                    _item.stat.name == 'special-defense' ? 'sp.def':
+                    _item.stat.name
+                }&nbsp;</td>
+                <td>{_item.base_stat}</td>
+              </tr>
+            ))}
+          </tbody>
+        : null}
+      </div>
+
     </li>
   );
 
@@ -91,6 +164,11 @@ function App() {
   useEffect(() => {
     if (window.initReady) loadMoreCards();
   }, [page]);
+  useEffect(() => {
+      searchCards()
+  }, [search]);
+
+
 
   useEffect(() => {
     visible < totalCard && totalCard > 0 && window.initReady
@@ -103,9 +181,34 @@ function App() {
 
 
   return (
-    <div className="App"
-    >
-      {listItems}
+    <div>
+      <div className="searchbar">
+        <input
+          type="text"
+          placeholder='Search by Name'
+          onChange={
+            (e)=>{
+              setSearch(e.target.value)
+            }
+          }
+        />
+      </div>
+
+      {
+        search.length>0 ?
+        <div className="App">
+          {
+            pokemonSearch !=null ?
+            resultItem()
+            :
+            'Nothing Exist.'
+          }
+        </div>
+        :
+        <div className="App">
+          {listItems}
+        </div>
+      }
     </div>
   );
 }
