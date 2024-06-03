e3c7d8d6dc7608f73e6e653008094688eb2ae1a7
pigmore <pigmore@gmail.com>
2024-06-03 15:23:10 +0800

    basic doom scrolling

--- src/App.js
+++ src/App.js
@@ -5,30 +5,72 @@ import axios from 'axios';
 
 function App() {
   const [pokemonList, setPokemonList] = useState([])
+  const [page, setPage] = useState(1)
+  const [visible, setVisible] = useState(0)
+  const [totalCard, setTotalCard] = useState(0)
 
   useEffect(() => {
     async function init() {
       if (typeof window !== "undefined" || window.initReady !== true) {
-        window.initReady = true
+
         axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`)
         .then(res => {
           const listTemp = res.data.results;
+
+          setTotalCard(res.data.count)
           setPokemonList(listTemp)
+          setVisible(listTemp.length)
+          window.initReady = true
         })
       }
     }
     init()
   }, [])
 
-  const listItems = pokemonList.map((item) =>
-    <li key={item.name} className='pokemonCard'>
+  const loadMoreCards = ()=>{
+    axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`)
+    .then(res => {
+      const listTemp = res.data.results;
+
+      setPokemonList((prev)=>[...prev,...listTemp])
+      setVisible((prev)=>prev +listTemp.length)
+    })
+  }
+
+  const listItems = pokemonList.map((item,index) =>
+    <li key={index} className='pokemonCard'>
       <img className='App-logo' src={logo} alt=""/>
       <br/>
     {item.name}</li>
   );
 
+  const handleOnScroll = () => {
+    if (
+      window.scrollY + window.innerHeight >=
+      document.documentElement.scrollHeight
+      && window.initReady
+    ) {
+      setPage((prev) => prev + 1);
+    }
+  };
+  useEffect(() => {
+    loadMoreCards();
+  }, [page]);
+
+  useEffect(() => {
+    visible < totalCard && totalCard > 0 && window.initReady
+      ? window.addEventListener("scroll", handleOnScroll)
+      : window.removeEventListener("scroll", handleOnScroll);
+    return () => {
+      window.removeEventListener("scroll", handleOnScroll);
+    };
+  }, [visible]);
+
+
   return (
-    <div className="App">
+    <div className="App"
+      onClick={loadMoreCards}
+    >
       {listItems}
     </div>
   );
