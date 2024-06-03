742fae4aa514c33997aa853dc0e32eaf7166b9b7
pigmore <pigmore@gmail.com>
2024-06-03 15:16:56 +0800

    basic grid css

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import { useRef, useState, useEffect,createRef} from 'react';
 import axios from 'axios';
 
-
 function App() {
+  const [pokemonList, setPokemonList] = useState([])
+
   useEffect(() => {
     async function init() {
       if (typeof window !== "undefined" || window.initReady !== true) {
@@ -12,15 +13,23 @@ function App() {
         axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`)
         .then(res => {
           const listTemp = res.data.results;
-          console.log(listTemp)
+          setPokemonList(listTemp)
         })
       }
     }
     init()
   }, [])
+
+  const listItems = pokemonList.map((item) =>
+    <li key={item.name} className='pokemonCard'>
+      <img className='App-logo' src={logo} alt=""/>
+      <br/>
+    {item.name}</li>
+  );
+
   return (
     <div className="App">
-
+      {listItems}
     </div>
   );
 }
